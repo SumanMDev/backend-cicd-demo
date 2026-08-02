7eeef05000068cfb604b1e2775c38691b3d6e37e
Suman <sumanmukherjee.business@gmail.com>
2026-08-02 09:41:54 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const PORT = 7000;
+
+app.get('/', (req, res) => {
+   res.status(200).send("<h1>Backend is running</h1>");
+})
+
+app.get('/health', (req, res) => {
+   res.send("Your app is running");
+})
+
+app.listen(PORT, ()=> {
+   console.log(`Your Server is running on http:localhost:${PORT}`)
+})
